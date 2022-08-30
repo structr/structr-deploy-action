2ad3d53b2c3d5f17edf742d5ddbcd358078fc57a
Lukas Reich <lukas.reich@structr.com>
2022-08-30 15:01:22 +0200

    Fixes deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ function healthCheck() {
     echo "Waiting for Structr to be ready for deployment"
     for i in $(seq 1 30)
     do
-      STATUS_CODE=$(curl --write-out --silent %{http_code} localhost:8082/structr/health/ready)
+      STATUS_CODE=$(curl --write-out %{http_code} --silent localhost:8082/structr/health/ready)
       
       if [ "$STATUS_CODE" -ne 200 ] ; then
         echo "Status is $STATUS_CODE. Still waiting for Structr to start..."
