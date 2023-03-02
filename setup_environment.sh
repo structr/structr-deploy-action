aaf740d4a8b637673d485ee3eeff7d1c3fd094d8
Lukas Reich <lukas.reich@structr.com>
2023-03-02 14:16:33 +0100

    Adds replacement for structr.conf file path.

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,3 +6,5 @@ sed -i "s|STRUCTR_WEBAPP_PATH|$STRUCTR_WEBAPP_PATH|g" docker-compose.yml &&
 if [ -z ${STRUCTR_CONF_FILE:-"./structr.conf"} ]; then
     touch ./structr.conf
 fi
+
+sed -i "s|STRUCTR_CONF_FILE|$STRUCTR_CONF_FILE|g" docker-compose.yml
