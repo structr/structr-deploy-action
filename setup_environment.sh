e2bbbd71e1a45e7f457b892ef30c0d8fae0854c2
Lukas Reich <lukas.reich@structr.com>
2023-03-02 14:33:41 +0100

    Adds more echo outputs.

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,3 +1,9 @@
+echo "Replacing variables for Structr and Neo4j docker configuration"
+echo Neo4j version: $NEO4j_VERSION
+echo Structr version: $STRUCTR_VERSION
+echo Structr license: $STRUCTR_LICENSE
+echo Structr webapp path: $STRUCTR_WEBAPP_PATH
+
 sed -i "s|NEO4j_VERSION|$NEO4j_VERSION|g" docker-compose.yml &&
 sed -i "s|STRUCTR_LICENSE|$STRUCTR_LICENSE|g" docker-compose.yml &&
 sed -i "s|STRUCTR_VERSION|$STRUCTR_VERSION|g" docker-compose.yml &&
@@ -7,4 +13,5 @@ if [ -z ${STRUCTR_CONF_FILE:-"./structr.conf"} ]; then
     touch ./structr.conf
 fi
 
+echo Structr conf file path: $STRUCTR_CONF_FILE
 sed -i "s|STRUCTR_CONF_FILE|$STRUCTR_CONF_FILE|g" docker-compose.yml
