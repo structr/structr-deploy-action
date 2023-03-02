ecacf933c70a6877c349e8840a85d3fa71f86c11
Lukas Reich <lukas.reich@structr.com>
2023-03-02 16:33:08 +0100

    Adds newline char in structr.conf and removes log output.

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,10 +2,6 @@ ACTION_PATH=$1
 cd $ACTION_PATH
 
 echo "Replacing variables for Structr and Neo4j docker configuration"
-echo Neo4j version: $NEO4j_VERSION
-echo Structr version: $STRUCTR_VERSION
-echo Structr license: $STRUCTR_LICENSE
-echo Structr webapp path: $STRUCTR_WEBAPP_PATH
 
 sed -i "s|NEO4j_VERSION|$NEO4j_VERSION|g" docker-compose.yml &&
 sed -i "s|STRUCTR_LICENSE|$STRUCTR_LICENSE|g" docker-compose.yml &&
@@ -16,6 +12,4 @@ if [ -z ${STRUCTR_CONF_FILE:-"./structr.conf"} ]; then
     touch ./structr.conf
 fi
 
-echo Structr conf file path: $STRUCTR_CONF_FILE
 sed -i "s|STRUCTR_CONF_FILE|$STRUCTR_CONF_FILE|g" docker-compose.yml
-cat docker-compose.yml
