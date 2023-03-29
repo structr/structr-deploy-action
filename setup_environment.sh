f225852844a058b99fcb896e5665650faf826513
Lukas Reich <derkaiserreich@users.noreply.github.com>
2023-03-29 13:37:58 +0200

    Adds fallback to structr conf variable.

--- setup_environment.sh
+++ setup_environment.sh
@@ -8,7 +8,8 @@ sed -i "s|STRUCTR_LICENSE|$STRUCTR_LICENSE|g" docker-compose.yml &&
 sed -i "s|STRUCTR_VERSION|$STRUCTR_VERSION|g" docker-compose.yml &&
 sed -i "s|STRUCTR_WEBAPP_PATH|$STRUCTR_WEBAPP_PATH|g" docker-compose.yml &&
 
-if [ -z ${STRUCTR_CONF_FILE:-"./structr.conf"} ]; then
+if [ -z "$STRUCTR_CONF_FILE" ]; then
+    STRUCTR_CONF_FILE="./structr.conf"
     touch ./structr.conf
 fi
 
